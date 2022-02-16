217717831cfbfaa554f282f111472654d8b195e3
Heinz Lätsch <heinz.laetsch@gmx.ch>
2022-02-16 09:10:25 +0100

    add

---
+++ anmeldetool-server/src/main/java/org/ztv/anmeldetool/models/RanglisteConfiguration.java
@@ -0,0 +1,31 @@
+package org.ztv.anmeldetool.models;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity()
+@Table(name = "RANGLISTEN_CONFIGURATION")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RanglisteConfiguration extends Base {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ANLASS_ID", nullable = false, insertable = true, updatable = true)
+	private Anlass anlass;
+
+	private KategorieEnum kategorie;
+
+	private TiTuEnum tiTu;
+
+	private int maxAuszeichnung;
+}
